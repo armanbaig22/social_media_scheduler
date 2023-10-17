39ffd6e6c45caa0213e1cd332aaffe5027579207
Arman baig <armanbaig012@gmail.com>
2023-10-17 11:00:54 +0530

    Working

--- core/forms.py
+++ core/forms.py
@@ -6,28 +6,21 @@ from django.utils import timezone
 class CreatePostForm(forms.Form):
     title = forms.CharField(max_length=100, label='Title', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
     description = forms.CharField(
-        label='Description',
-        widget=forms.Textarea(attrs={'class': 'form-control mb-3', 'rows': 4}),
-        required=False,
-    )
+    label='Description',
+    widget=forms.Textarea(attrs={'class': 'form-control mb-3', 'rows': 4}),
+    required=False,  # If applicable
+)
 
     media = forms.FileField(label='Media (Image/Video)', required=False, widget=forms.ClearableFileInput(attrs={'class': 'form-control mb-3'}))
-    
-    POST_TYPE_CHOICES = [('draft', 'Draft'), ('schedule', 'Schedule')]
     post_type = forms.ChoiceField(
-        choices=POST_TYPE_CHOICES,
+        choices=[('draft', 'Draft'), ('schedule', 'Schedule')],
         initial='draft',
         label='Post Type',
-        widget=forms.Select(attrs={'class': 'form-control mb-3'}),
+        widget=forms.Select(attrs={'class': 'form-control mb-3'})
     )
-
-    schedule_datetime = forms.SplitDateTimeField(
-        label='Schedule Date & Time',
-        initial=timezone.now(),
-        widget=forms.SplitDateTimeWidget(
-            date_attrs={'type': 'date', 'class': 'form-control mb-3'},
-            time_attrs={'type': 'time', 'class': 'form-control mb-3'},
-        )
+    schedule_datetime = forms.DateTimeField(
+        widget=forms.TextInput(attrs={'type': 'datetime-local', 'class': 'form-control mb-3'}),
+        label='Schedule Date & Time'
     )
 
     def clean_schedule_datetime(self):
@@ -52,15 +45,6 @@ class PostForm(forms.ModelForm):
 
         # Update the 'status' field choices
         self.fields['status'].choices = status_choices
-
-        # Adjust the 'rows' attribute of the content field
-        self.fields['content'].widget.attrs['rows'] = 2
-
-        # Add date and time widget to the 'scheduled_datetime' field
-        self.fields['scheduled_datetime'].widget = forms.SplitDateTimeWidget(
-            date_attrs={'type': 'date', 'class': 'form-control mb-3'},
-            time_attrs={'type': 'time', 'class': 'form-control mb-3'},
-        )
 
     def clean(self):
         cleaned_data = super().clean()
